c59380f088af54f8079bf1b3402c71102ac2c28d
Maycolx <xmaycol.2020.5@gmail.com>
2023-06-19 01:24:31 -0500

    delete commit

--- src/test/java/runner/testRunner.java
+++ src/test/java/runner/testRunner.java
@@ -1,14 +1,14 @@
 package runner;
 
-import com.intuit.karate.junit5.Karate;
+//import com.intuit.karate.junit5.Karate;
 
 public class testRunner {
-    @Karate.Test
-    Karate getAPITest() {
-        return Karate.run("../features/poke").relativeTo(getClass());
-    }
-    @Karate.Test
-    Karate getAPITest2() {
-        return Karate.run("../features/json").relativeTo(getClass());
-    }
+//    @Karate.Test
+//    Karate getAPITest() {
+//        return Karate.run("../features/poke").relativeTo(getClass());
+//    }
+//    @Karate.Test
+//    Karate getAPITest2() {
+//        return Karate.run("../features/json").relativeTo(getClass());
+//    }
 }
